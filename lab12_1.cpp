43d2e698e514651e9a16b8e7341e2cca7468db84
Pakwannn <pkpkearth@gmail.com>
2024-12-24 14:42:25 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
+long long int fibonacci(long long);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(long long int x){
+    if(x <= 1){
+        return x;
+    }else if(x > 1){
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
+return -1;
 }
